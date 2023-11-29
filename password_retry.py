1509214b0a69ef6e8dfb3c6aaa1095f68938c3d9
akomito357 <ariel860115@gmail.com>
2023-11-29 21:55:47 +0800

    change to if chance > 0

--- password_retry.py
+++ password_retry.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 chance = 3
 while chance > 0:
+    chance = chance - 1
     input_password = input('請輸入密碼：')
     if input_password == password:
         print('登入成功！')
         break
-    else:
-        chance = chance - 1
-        print('密碼錯誤！還有', chance, '次機會')
+    else: 
+        print('密碼錯誤！')
+        if chance > 0:
+            print('還有', chance, '次機會')
+        else:
+            print('沒機會嘗試了！要鎖帳號了啦！')
